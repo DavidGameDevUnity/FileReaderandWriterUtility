85e34d061cc40b72b60f4a31364ae52345358433
DavidGameDevUnity <c38554372@gmail.com>
2026-02-23 20:29:33 +0300

    удалил лишние строки

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace FileReaderandWriterUtility
         {
             Reader reader;
             Writer writer;
-<<<<<<< HEAD
+
             if (args.Length != 0 && args[1] == "-r")
             {
                 reader = new Reader(args[0]);
@@ -19,28 +19,28 @@ namespace FileReaderandWriterUtility
             {
                 writer = new Writer(args[0]);
                 writer.Write(args[2]);
-=======
-            if (args.Length >= 2)
-            {
-                if (args[1] == "-r")
+
+                if (args.Length >= 2)
                 {
-                    reader = new Reader(args[0]);
-                    Console.WriteLine(reader.Read());
-                }
-                else if (args.Length >= 3 && args[1] == "-w")
-                {
-                    writer = new Writer(args[0]);
-                    writer.Write(args[2]);
+                    if (args[1] == "-r")
+                    {
+                        reader = new Reader(args[0]);
+                        Console.WriteLine(reader.Read());
+                    }
+                    else if (args.Length >= 3 && args[1] == "-w")
+                    {
+                        writer = new Writer(args[0]);
+                        writer.Write(args[2]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы забыли записать свой текст или выбрать режим -r или -w");
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("Вы забыли записать свой текст или выбрать режим -r или -w");
+                    Console.WriteLine("Вы забыли указать имя файла!");
                 }
->>>>>>> fix arguments bugs
-            }
-            else
-            {
-                Console.WriteLine("Вы забыли указать имя файла!");
             }
         }
     }
